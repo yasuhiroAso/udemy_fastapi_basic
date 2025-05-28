d384c71ba481145717dd2f5b0a550f55b805cee8
yasuhiro.aso <yasuhiro.aso@lasfactory.com>
2025-05-28 14:46:52 +0900

    55.ルーティングの雛形を作る（途中）

---
+++ routers/contact.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/contacts")
+async def get_contact_all():
+    pass
+
+
+@router.post("/contacts")
+async def create_contact():
+    pass
+
+
+@router.get("/contacts/{id}")
+async def get_contact():
+    pass
+
+
+@router.put("/contacts/{id}")
+async def update_contact():
+    pass
+
+
+@router.delete("/contacts{id}")
+async def delete_contact():
+    pass
