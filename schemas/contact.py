89d351588f2190a847c3cd37c1525b73d033397e
yasuhiro.aso <yasuhiro.aso@lasfactory.com>
2025-05-28 15:51:44 +0900

    Fix_58

--- schemas/contact.py
+++ schemas/contact.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr, HttpUrl
 from datetime import datetime
 
 
 class Contact(BaseModel):  # Pydantic継承することでスキーマ定義が可能
     id: int
-    name: str
-    email: str
-    url: str
-    gender: int
-    message: str
-    is_enabled: bool
+    name: str = Field(..., min_length=2, max_length=50)
+    email: EmailStr
+    url: HttpUrl | None = Field(default=None)
+    gender: int = Field(..., strict=True, ge=0, le=2)
+    message: str = Field(..., max_length=200)
+    is_enabled: bool = Field(default=False)
     created_at: datetime
